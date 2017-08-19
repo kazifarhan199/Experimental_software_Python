f0f05bb500197556bf8f68b16126275746307255
kazifarhan <kazifarhan199@gmmail.com>
2017-08-20 00:41:28 +0530

    Updated

--- Test_experiments/main.py
+++ Test_experiments/main.py
@@ -18,23 +18,28 @@ def display_data():
 	global date_of_transition, name_of_transition, amount_of_transition, amount_of_balance 
 	for i in range(len(date_of_transition)):
 		
-		dates_labels.append(Label(frame1,text=date_of_transition[i]))
+		dates_labels.append(Label(frame1,bg='snow',text=date_of_transition[i]))
 		dates_labels[i].grid(row=i+2,column=0)
+		Label(frame1,text='|',bg='snow').grid(row=i+2,column=1)
+		names_labels.append(Label(frame1,bg='snow',text=name_of_transition[i]))
+		names_labels[i].grid(row=i+2,column=2)
 
-		names_labels.append(Label(frame1,text=name_of_transition[i]))
-		names_labels[i].grid(row=i+2,column=1)
+		amount_labels.append(Label(frame1,bg='snow',text=amount_of_transition[i]))
+		amount_labels[i].grid(row=i+2,column=3)
+		Label(frame1,text='|',bg='snow').grid(row=i+2,column=4)
+		balance_labels.append(Label(frame1,bg='snow',text=amount_of_balance[i]))
+		balance_labels[i].grid(row=i+2,column=5)
+		Label(frame1,text='|',bg='snow').grid(row=i+2,column=6)
+		edit_buttons.append(Button(frame1,text='EDIT',bg='ivory3',width=12,command=partial(edit,i,date_of_transition, name_of_transition, amount_of_transition)))
+		edit_buttons[i].grid(row=i+2,column=7)
 
-		amount_labels.append(Label(frame1,text=amount_of_transition[i]))
-		amount_labels[i].grid(row=i+2,column=2)
+		deleat_buttons.append(Button(frame1,text='DELEAT',width=12,bg='ivory3',command=partial(terminate,i)))
+		deleat_buttons[i].grid(row=i+2,column=8)
 
-		balance_labels.append(Label(frame1,text=amount_of_balance[i]))
-		balance_labels[i].grid(row=i+2,column=3)
-
-		edit_buttons.append(Button(frame1,text='EDIT',command=partial(edit,i,date_of_transition, name_of_transition, amount_of_transition)))
-		edit_buttons[i].grid(row=i+2,column=4)
-
-	show_balance_label.append(Label(frame2, fg='green',font=(None,17)))
+	show_balance_label.append(Label(frame2, fg='green',bg='snow',font=(None,17)))
 	show_balance_label[0].grid(row=1,column=2)
+
+
 	try:
 		show_balance_label[0].configure(text=amount_of_balance[-1])
 	except:
@@ -45,9 +50,9 @@ def edit(i,date_of_transition, name_of_transition, amount_of_transition):
 	global event
 
 	if event==0:
-		edit_date_containor=Entry(frame1,width=9)
-		edit_name_containor=Entry(frame1,width=10)
-		edit_amount_containor=Entry(frame1,width=7)
+		edit_date_containor=Entry(frame1,width=9,bg='snow')
+		edit_name_containor=Entry(frame1,width=10,bg='snow')
+		edit_amount_containor=Entry(frame1,width=7,bg='snow')
 		#edit_balance_containor=Entry(frame1,width=10)	
 
 		edit_date_containor.grid(row=i+2,column=0)
@@ -93,13 +98,13 @@ def save(i,get_date,get_name,get_amount,edit_date_containor,edit_name_containor,
 
 def add_transition():
 	master=Tk()
-	Label(master,text='Enter Transition name : ').grid(row=0,column=0)
-	Label(master,text='Enter Transition amount : ').grid(row=1,column=0)
-	new_name_to_add=Entry(master)
-	new_amount_to_add=Entry(master)
+	Label(master,bg='snow',text='Enter Transition name : ').grid(row=0,column=0)
+	Label(master,bg='snow',text='Enter Transition amount : ').grid(row=1,column=0)
+	new_name_to_add=Entry(master,bg='snow')
+	new_amount_to_add=Entry(master,bg='snow')
 	new_name_to_add.grid(row=0,column=1)
 	new_amount_to_add.grid(row=1,column=1)
-	Button(master,text='Add',command=lambda:add_transition_main(new_name_to_add.get(),new_amount_to_add.get(),master),width=20).grid(row=2,column=1)
+	Button(master,text='Add',bg='snow',command=lambda:add_transition_main(new_name_to_add.get(),new_amount_to_add.get(),master),width=20).grid(row=2,column=1)
 	master.mainloop()
 
 def add_transition_main(name,amount,master):
@@ -118,14 +123,6 @@ def add_transition_main(name,amount,master):
 
 	display_data()
 	master.destroy()
-
-def deleater():
-	global date_of_transition
-	Label(frame3,text='Deleat').grid(row=0,column=0)
-	Label(frame3,text='___________').grid(row=1,column=0)
-	for i in range(len(date_of_transition)):
-		deleat_buttons.append(Button(frame3,text='D',bg='black',fg='red',command=partial(terminate,i)))
-		deleat_buttons[i].grid(row=i+2,column=0)
 
 def terminate(i):
 	global date_of_transition, name_of_transition, amount_of_transition, amount_of_balance
@@ -150,30 +147,41 @@ def terminate(i):
 	edit_buttons[i].destroy()
 	deleat_buttons[i].destroy()
 
+	del dates_labels[i]
+	del names_labels[i]
+	del amount_labels[i]
+	del balance_labels[i]
+	del edit_buttons[i]
+	del deleat_buttons[i]
+
 	show_balance_label[0].configure(text=amount_of_balance[-1])
+	
+	print(str(i)+'this is i')
+	
+	for r in range(len(balance_labels)):
+		balance_labels[r].configure(text=amount_of_balance[r])
+		print(r)
 
 root=Tk()
 root.resizable(False,False)	
-frame1 = Frame()
+frame1 = Frame(bg='snow')
 frame1.grid(row=0,column=0)
 
 
-Label(frame1,text='------------Date-------------Transitin Type---------------Transition Ammount-----------Balance-----------EDIT---------------').grid(row=0,column=0,columnspan=5)
-Label(frame1,text='____________________________________________________________________________________________________________________________________________').grid(row=1,column=0,columnspan=5)
+Label(frame1,bg='snow',text='----------------Date------------------Transitin Type-------------Transition Ammount------------------Balance------------------------------------EDIT----------------------------DELEAT-------------').grid(row=0,column=0,columnspan=9)
+Label(frame1,bg='snow',text='_______________________________________________________________________________________________________________________________________________________________________________________________________________').grid(row=1,column=0,columnspan=9)
 
-frame2 = Frame()
+frame2 = Frame(bg='snow')
 frame2.grid(row=1,column=0,columnspan=2)
 
-frame3 = Frame()
-frame3.grid(row=0,column=1)
+
+framebin = Frame()
 
 display_data()
 
-Label(frame2,text="______________________________________________________________________________________________________________________________________________").grid(row=0,column=0,columnspan=3)
-Add_transition_button=Button(frame2,text='ADD TRANSITION',font=(None,13),bg='blue',command=lambda:add_transition())
+Label(frame2,bg='snow',text="________________________________________________________________________________________________________________________________________________________________________________________________________").grid(row=0,column=0,columnspan=5)
+Add_transition_button=Button(frame2,font=('Arial CE',13),text='ADD TRANSITION',bg='ivory3',command=lambda:add_transition())
 Add_transition_button.grid(row=1,column=0)
-
-deleater()
 
 root.bind('<Control-w>',exit)
 root.mainloop()
